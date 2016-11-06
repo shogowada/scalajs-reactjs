351039f5d2bd6c0ec967a33aa020a9b68a5826d7
Shogo Wada <shogowada@users.noreply.github.com>
2016-11-05 21:25:38 -0500

    Support all elements for synthetic event

--- core/src/main/scala/io/github/shogowada/scalajs/reactjs/events/SyntheticEvent.scala
+++ core/src/main/scala/io/github/shogowada/scalajs/reactjs/events/SyntheticEvent.scala
@@ -1,21 +1,17 @@
 package io.github.shogowada.scalajs.reactjs.events
 
-import io.github.shogowada.scalajs.reactjs.elements.ReactHTMLInputElement
+import io.github.shogowada.scalajs.reactjs.elements._
 
 import scala.scalajs.js
 
 @js.native
 trait SyntheticEvent extends js.Object {
 
-  def bubbles: Boolean = js.native
-
-  def cancelable: Boolean = js.native
-
-  def defaultPrevented: Boolean = js.native
-
-  def eventPhase: Int = js.native
-
-  def isTrusted: Boolean = js.native
+  val bubbles: Boolean = js.native
+  val cancelable: Boolean = js.native
+  val defaultPrevented: Boolean = js.native
+  val eventPhase: Int = js.native
+  val isTrusted: Boolean = js.native
 
   def preventDefault(): Unit = js.native
 
@@ -25,12 +21,26 @@ trait SyntheticEvent extends js.Object {
 
   def isPropagationStopped(): Boolean = js.native
 
-  def timeStamp: Long = js.native
-
-  def `type`: String = js.native
+  val timeStamp: Long = js.native
+  val `type`: String = js.native
 }
 
 @js.native
-trait InputElementSyntheticEvent extends SyntheticEvent {
-  def target: ReactHTMLInputElement = js.native
+trait ElementSyntheticEvent[ELEMENT <: ReactHTMLElement] extends SyntheticEvent {
+  val target: ReactHTMLInputElement = js.native
 }
+
+@js.native
+trait CheckBoxElementSyntheticEvent extends ElementSyntheticEvent[ReactHTMLCheckBoxElement]
+
+@js.native
+trait InputElementSyntheticEvent extends ElementSyntheticEvent[ReactHTMLInputElement]
+
+@js.native
+trait OptionElementSyntheticEvent extends ElementSyntheticEvent[ReactHTMLOptionElement]
+
+@js.native
+trait RadioElementSyntheticEvent extends ElementSyntheticEvent[ReactHTMLRadioElement]
+
+@js.native
+trait TextAreaElementSyntheticEvent extends ElementSyntheticEvent[ReactHTMLTextAreaElement]
