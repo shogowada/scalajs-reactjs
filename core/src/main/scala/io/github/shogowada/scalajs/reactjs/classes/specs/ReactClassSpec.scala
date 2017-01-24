5183829988f6bfcbb1e0b8b48f60fcc4bea06f6e
Shogo Wada <shogowada@users.noreply.github.com>
2017-01-24 06:30:54 -0600

    Add lifecycle methods
    It is failing on router test because router uses js.native object as props, while core assumes it's Scala case classes. We need to do something about it.

--- core/src/main/scala/io/github/shogowada/scalajs/reactjs/classes/specs/ReactClassSpec.scala
+++ core/src/main/scala/io/github/shogowada/scalajs/reactjs/classes/specs/ReactClassSpec.scala
@@ -11,17 +11,43 @@ trait ReactClassSpec {
   type Props
   type State
 
+  def props: Props = nativeThis.props.asScala[Props]
+
   def state: State = nativeThis.state.asScala[State]
 
-  def props: Props = nativeThis.props.asScala[Props]
+  def componentWillMount(): Unit = {}
+
+  def componentDidMount(): Unit = {}
+
+  def componentWillReceiveProps(nextProps: Props): Unit = {}
+
+  def shouldComponentUpdate(nextProps: Props, nextState: State): Boolean = {
+    true
+  }
+
+  def componentWillUpdate(nextProps: Props, nextState: State): Unit = {}
+
+  def componentDidUpdate(prevProps: Props, prevState: State): Unit = {}
+
+  def componentWillUnmount(): Unit = {}
+
+  def forceUpdate(callback: js.Function0[Unit]): Unit = nativeThis.forceUpdate(callback)
+
+  def forceUpdate(): Unit = nativeThis.forceUpdate()
 
   def getInitialState(): State
 
   def setState(state: State): Unit = nativeThis.setState(state.asJs)
 
   def setState(stateMapper: State => State): Unit = {
-    val nativeStateMapper: js.Function1[js.Any, js.Any] =
-      (state: js.Any) => stateMapper(state.asScala[State]).asJs
+    val nativeStateMapper: js.Function1[js.Object, js.Any] =
+      (prevState: js.Object) => stateMapper(prevState.asScala[State]).asJs
+    nativeThis.setState(nativeStateMapper)
+  }
+
+  def setState(stateMapper: (State, Props) => State): Unit = {
+    val nativeStateMapper: js.Function2[js.Object, js.Object, js.Any] =
+      (prevState: js.Object, props: js.Object) => stateMapper(prevState.asScala[State], props.asScala[Props]).asJs
     nativeThis.setState(nativeStateMapper)
   }
 
@@ -33,7 +59,35 @@ trait ReactClassSpec {
 
   def nativeThis: js.Dynamic = _nativeThis
 
-  def asNative = js.Dynamic.literal(
+  def asNative: js.Dynamic = js.Dynamic.literal(
+    "componentWillMount" -> js.ThisFunction.fromFunction1((newNativeThis: js.Dynamic) => {
+      _nativeThis = newNativeThis
+      componentWillMount()
+    }),
+    "componentDidMount" -> js.ThisFunction.fromFunction1((newNativeThis: js.Dynamic) => {
+      _nativeThis = newNativeThis
+      componentDidMount()
+    }),
+    "componentWillReceiveProps" -> js.ThisFunction.fromFunction2((newNativeThis: js.Dynamic, nextProps: js.Object) => {
+      _nativeThis = newNativeThis
+      componentWillReceiveProps(nextProps.asScala[Props])
+    }),
+    "shouldComponentUpdate" -> js.ThisFunction.fromFunction3((newNativeThis: js.Dynamic, nextProps: js.Object, nextState: js.Object) => {
+      _nativeThis = newNativeThis
+      shouldComponentUpdate(nextProps.asScala[Props], nextState.asScala[State])
+    }),
+    "componentWillUpdate" -> js.ThisFunction.fromFunction3((newNativeThis: js.Dynamic, nextProps: js.Object, nextState: js.Object) => {
+      _nativeThis = newNativeThis
+      componentWillUpdate(nextProps.asScala[Props], nextState.asScala[State])
+    }),
+    "componentDidUpdate" -> js.ThisFunction.fromFunction3((newNativeThis: js.Dynamic, prevProps: js.Object, prevState: js.Object) => {
+      _nativeThis = newNativeThis
+      componentDidUpdate(prevProps.asScala[Props], prevState.asScala[State])
+    }),
+    "componentWillUnmount" -> js.ThisFunction.fromFunction1((newNativeThis: js.Dynamic) => {
+      _nativeThis = newNativeThis
+      componentWillUnmount()
+    }),
     "getInitialState" -> js.ThisFunction.fromFunction1((newNativeThis: js.Dynamic) => {
       _nativeThis = newNativeThis
       Option(getInitialState()).map(_.asJs).orNull
