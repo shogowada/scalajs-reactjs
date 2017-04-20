14cac36b673dc671e468151748369087e53f8301
Shogo Wada <shogowada@users.noreply.github.com>
2017-04-19 20:01:38 -0500

    Refactor

--- example/helloworld-function/src/main/scala/io/github/shogowada/scalajs/reactjs/example/helloworldfunction/Main.scala
+++ example/helloworld-function/src/main/scala/io/github/shogowada/scalajs/reactjs/example/helloworldfunction/Main.scala
@@ -2,7 +2,6 @@ package io.github.shogowada.scalajs.reactjs.example.helloworldfunction
 
 import io.github.shogowada.scalajs.reactjs.ReactDOM
 import io.github.shogowada.scalajs.reactjs.VirtualDOM._
-import io.github.shogowada.scalajs.reactjs.elements.ReactElement
 import org.scalajs.dom
 
 import scala.scalajs.js.JSApp
@@ -10,7 +9,7 @@ import scala.scalajs.js.JSApp
 object Main extends JSApp {
   def main(): Unit = {
     object HelloWorld {
-      def apply(name: String): ReactElement = <.div(^.id := "hello-world")(s"Hello, ${name}!")
+      def apply(name: String) = <.div(^.id := "hello-world")(s"Hello, ${name}!")
     }
 
     val mountNode = dom.document.getElementById("mount-node")
