a449482b4f1df30d921e7ee4c9e7e93977d5aade
Shogo Wada <shogowada@users.noreply.github.com>
2017-04-22 09:34:02 -0500

    Ignore unexpected alerts

--- example/test/src/it/scala/io/github/shogowada/scalajs/reactjs/example/BaseTest.scala
+++ example/test/src/it/scala/io/github/shogowada/scalajs/reactjs/example/BaseTest.scala
@@ -1,12 +1,18 @@
 package io.github.shogowada.scalajs.reactjs.example
 
+import org.openqa.selenium.UnexpectedAlertBehaviour
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.remote.{CapabilityType, DesiredCapabilities}
 import org.scalatest.concurrent.Eventually
 import org.scalatest.selenium.{Driver, WebBrowser}
 import org.scalatest.{Matchers, path}
 
 object BaseTest {
-  val webDriver = new ChromeDriver()
+  val webDriver = {
+    val capabilities = new DesiredCapabilities()
+    capabilities.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE)
+    new ChromeDriver()
+  }
 
   Runtime.getRuntime.addShutdownHook(new Thread(() => webDriver.quit()))
 }
